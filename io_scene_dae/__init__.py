be161d9f328d60d27023b93e9c21cb33318cc9de
David Lejeune <daffydlejeune42@gmail.com>
2015-03-10 18:20:34 -0700

    Updated _init_.py to differentiate from the original Better Collada Exporter in Blender menus.

--- io_scene_dae/__init__.py
+++ io_scene_dae/__init__.py
@@ -19,14 +19,14 @@
 # <pep8-80 compliant>
 
 bl_info = {
-    "name": "Better Collada Exporter",
-    "author": "Juan Linietsky",
+    "name": "Better Collada Exporter - HW Edition",
+    "author": "Juan Linietsky, David Lejeune",
     "blender": (2, 5, 8),
     "api": 38691,
     "location": "File > Import-Export",
-    "description": ("Export DAE Scenes, This plugin actually works better! otherwise contact me."),
+    "description": ("Export DAE Scenes to be compatible with the HODOR tool for Homeworld Remastered."),
     "warning": "",
-    "wiki_url": ("http://www.godotengine.org"),
+    "wiki_url": (""),
     "tracker_url": "",
     "support": 'OFFICIAL',
     "category": "Import-Export"}
@@ -180,7 +180,7 @@ class ExportDAE(bpy.types.Operator, ExportHelper):
 
 
 def menu_func(self, context):
-    self.layout.operator(ExportDAE.bl_idname, text="Better Collada (.dae)")
+    self.layout.operator(ExportDAE.bl_idname, text="Better Collada - HW (.dae)")
 
 
 def register():
